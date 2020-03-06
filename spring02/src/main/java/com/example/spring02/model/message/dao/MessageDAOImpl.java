4e37fe3b51159999127300c792807ec856b7bd89
qnwnen22 <57924805+qnwnen22@users.noreply.github.com>
2020-03-06 14:28:36 +0900

    0306 AOP 실습

---
+++ spring02/src/main/java/com/example/spring02/model/message/dao/MessageDAOImpl.java
@@ -0,0 +1,33 @@
+package com.example.spring02.model.message.dao;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.example.spring02.model.message.dto.MessageDTO;
+
+@Repository
+public class MessageDAOImpl implements MessageDAO {
+
+	@Inject //의존관계 주입시 2개이상이면 따로 처리
+	SqlSession sqlSession;
+	
+	@Override
+	public void create(MessageDTO dto) {
+		sqlSession.insert("message.create",dto);
+	}
+
+	@Override
+	public MessageDTO readMessage(int mid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateState(int mid) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
